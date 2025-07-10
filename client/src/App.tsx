623c865ef75dedcda2b94c1417907bd328c92812
LUCIFER881DEVIL <lucidev881il@gmail.com>
2025-07-10 17:07:19 +0530

    Modify App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -74,7 +74,7 @@ function App() {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:8000/analyze', {
+      const response = await fetch('https://emotion-reflection-tool-1.onrender.com/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text }),
